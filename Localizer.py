7f7a18eefb1006ed0510a64bd2b52033cc7c3094
David Grier <david.grier@nyu.edu>
2019-01-31 20:03:51 -0500

    reorganizing for clarity

--- Localizer.py
+++ Localizer.py
@@ -1,7 +1,10 @@
-import numpy as np
-import json
-from darknet import *
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import darknet
 from pylorenzmie.theory.Instrument import Instrument
+import json
+
 
 class Localizer(object):
     '''
@@ -10,11 +13,13 @@ class Localizer(object):
     net: network
     meta: metadata
     instrument: Instrument
-        Object resprenting the light-scattering instrument                                                                                                                         
+        Object representing the light-scattering instrument
+
     Methods
     _______
-    predict(img_list, save_to_json, predictions_path)'''
-    
+    predict(img_list, save_to_json, predictions_path)
+    '''
+
     def __init__(self,
                  config_path='',
                  meta_path='',
@@ -24,7 +29,8 @@ class Localizer(object):
             self.instrument = Instrument()
         else:
             self.instrument = instrument
-        self.net, self.meta = instantiate(config_path, weight_path, meta_path)
+        self.net, self.meta = darknet.instantiate(
+            config_path, weight_path, meta_path)
 
     @property
     def instrument(self):
@@ -50,9 +56,11 @@ class Localizer(object):
     def meta(self, meta):
         self._meta = meta
 
-
-    def predict(self, img_list = [], thresh = 0.5,
-                save_to_json=False, predictions_path='yolo_predictions.json'):
+    def predict(self,
+                img_list=[],
+                thresh=0.5,
+                save_to_json=False,
+                predictions_path='yolo_predictions.json'):
         '''
         input:
         img_list: list of images (ie cv2.imread('image.png'))
@@ -60,43 +68,47 @@ class Localizer(object):
         (predictions_path does nothing otherwise)
         thresh: int
         threshold for detection
-        
+
         output:
         predictions: list of list of dicts
         n images => n lists of dicts
         per holo prediction:
              {'conf': 50%, 'bbox': (x_centroid, y_centroid, width, height)}
         '''
-        
+
         predictions = []
         for image in img_list:
-            yolopred = detect(self.net, self.meta, image, thresh)
+            yolopred = darknet.detect(self.net, self.meta, image, thresh)
             imagepreds = []
             for pred in yolopred:
-                (label,conf,bbox) = pred
+                (label, conf, bbox) = pred
                 imagepreds.append({'conf': conf, 'bbox': bbox})
             predictions.append(imagepreds)
-        
+
         if save_to_json:
             with open(predictions_path, 'w') as outfile:
                 json.dump(predictions, outfile)
         return predictions
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
+    import cv2
+
     config_path = 'cfg_darknet/holo.cfg'
     weight_path = 'cfg_darknet/holo_55000.weights'
     meta_path = 'cfg_darknet/holo.data'
-    localizer = Localizer(config_path = config_path, weight_path = weight_path, meta_path = meta_path)
-    import cv2
+    localizer = Localizer(config_path=config_path,
+                          weight_path=weight_path, meta_path=meta_path)
     img_file = 'examples/test_image_large.png'
     test_img = cv2.imread(img_file)
     pred_path = 'examples/test_yolo_pred.json'
-    detection = localizer.predict(img_list = [test_img], save_to_json=True, predictions_path=pred_path)
+    detection = localizer.predict(
+        img_list=[test_img], save_to_json=True, predictions_path=pred_path)
     example = detection[0]
     for holo in example:
-        (x,y,w,h) = holo['bbox']
+        (x, y, w, h) = holo['bbox']
         conf = holo['conf']*100
-        x = round(x,3)
-        y = round(y,3)
+        x = round(x, 3)
+        y = round(y, 3)
         conf = round(conf)
-        print('Hologram detected at ({},{}) with {}% confidence.'.format(x,y,conf))
+        print('Hologram detected at ({},{}) with {}% confidence.'.format(x, y, conf))
