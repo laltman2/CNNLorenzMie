e28c0fe213777a931f4c0033ec480c89742c112b
laltman2 <laurenealtman@gmail.com>
2019-12-18 14:54:03 -0500

    experiment scripts cleaned up and use feature serialization

--- experiments/predict.py
+++ experiments/predict.py
@@ -16,7 +16,7 @@ img_eval = normalize_video(bkg_path, vid_path, save_folder='./norm_images/')
 
 
 
-keras_head_path = '/home/group/endtoend/OOe2e/keras_models/predict_stamp_fullrange_adamnew_extnoise_lowscale'
+keras_head_path = '../keras_models/predict_stamp_best'
 keras_model_path = keras_head_path+'.h5'
 keras_config_path = keras_head_path+'.json'
 with open(keras_config_path, 'r') as f:
@@ -24,7 +24,7 @@ with open(keras_config_path, 'r') as f:
 estimator = Estimator(model_path=keras_model_path, config_file=kconfig)
 
     
-localizer = Localizer(configuration = 'yolonew', weights='_100000')
+localizer = Localizer(configuration = 'holo', weights='_100k')
 
 e2e = EndtoEnd(estimator=estimator, localizer=localizer)
 
@@ -38,49 +38,13 @@ for i in range(numimgs):
     filepath = './norm_images/image' + str(i).zfill(4) + '.png'
     localim = cv2.imread(filepath)
     features = e2e.predict(img_list = [localim])[0]
-    counter = 0
     for feature in features:
-        counter += 1
-        localdict = {'framenum':i}
-        particle = feature.model.particle
-        localdict['x_p'] = particle.x_p
-        localdict['y_p'] = particle.y_p
-        localdict['z_p'] = particle.z_p
-        localdict['a_p'] = particle.a_p
-        localdict['n_p'] = particle.n_p
-        extsize = feature.data.size
-        extpx = int(np.sqrt(extsize))
-        localdict['ext'] = extpx
+        localdict = feature.serialize(exclude=['data'])
+        localdict['framenum'] = i
+        localdict['framepath'] = filepath
         savedict.append(localdict)
     print('Completed frame {}'.format(i), end='\r')
 
 with open('your_MLpreds.json', 'w') as f:
     json.dump(savedict, f)
 print('saved ML')
-
-
-firstimagepreds = features
-firstimagepath = 'norm_images/image{}.png'.format(str(numimgs-1).zfill(4))
-firstimg = cv2.imread(firstimagepath)
-fig, ax = plt.subplots()
-ax.imshow(firstimg, cmap='gray')
-for feature in firstimagepreds:
-    x = feature.model.particle.x_p
-    y = feature.model.particle.y_p
-    ax.scatter([x],[y], color='red')
-plt.show()
-
-example = features[0]
-
-print('Example feature')
-print(example.model.particle)
-px = int(np.sqrt(example.data.size))
-pix = (px,px)
-cpix = estimator.pixels
-    
-h = example.model.hologram()
-fig, (ax1, ax2) = plt.subplots(1,2)
-ax1.imshow(np.clip(example.data.reshape(pix)*60, 0, 255), cmap='gray')
-ax2.imshow(np.clip(h.reshape(pix)*60, 0, 255), cmap='gray')
-fig.suptitle('Data, Predicted Hologram')
-plt.show()
--- experiments/refine.py
+++ experiments/refine.py
@@ -19,77 +19,31 @@ savedict = []
 for i in range(numimgs):
     filepath = './norm_images/image' + str(i).zfill(4) + '.png'
     localim = cv2.imread(filepath)
-    localpreds = [x for x in MLpreds if x['x_p'] > 750 and x['framenum']==i]
+    localpreds = [x for x in MLpreds if x['framenum']==i]
     #reformat for cropping
     for pred in localpreds:
         localxy = {"conf":1}
         x_p = pred['x_p']
         y_p = pred['y_p']
-        ext = pred['ext']
+        ext = pred['shape'][0]
         localxy["bbox"] = [x_p, y_p, ext, ext]
         features,_,_ = crop_feature(img_list = [localim], xy_preds = [[localxy]])
+        #instatiates a feature, puts in data, coords, x_p, y_p
         if len(features[0]) != 1:
             print('Something went wrong')
             print(len(features[0]))
-        feature = features[0][0]
-        p = feature.model.particle
-        p.z_p = pred['z_p']
-        p.a_p = pred['a_p']
-        p.n_p = pred['n_p']
-        ins = feature.model.instrument
-        ins.wavelength = 0.447
-        ins.magnification = 0.048
-        ins.n_m = 1.34
-        feature.model.coordinates = feature.coordinates
+        feature = features[0][0].deserialize(pred) #puts in rest of feature info
         start = time()
         result = feature.optimize(method='ameoba-lm')
         print("Time to fit: {:03f}".format(time() - start))
-        redchi = (result.fun).dot(result.fun) / (result.fun.size - result.x.size)
-        localdict = {'framenum':i}
-        particle = feature.model.particle
-        localdict['x_p'] = particle.x_p
-        localdict['y_p'] = particle.y_p
-        localdict['z_p'] = particle.z_p
-        localdict['a_p'] = particle.a_p
-        localdict['n_p'] = particle.n_p
-        localdict['redchi'] = redchi
+        print(result)
+        localdict = feature.serialize(exclude=['data'])
+        localdict['framenum'] = i
+        localdict['framepath'] = filepath
+        localdict['redchi'] = result.redchi
         savedict.append(localdict)
-        z_last = particle.z_p
-        pix = (ext, ext)
-        #cropdata = feature.data.reshape(pix)*100
-        #fname= './crops/image{}.png'.format(str(i).zfill(4))
-        #print(fname)
-        #cv2.imwrite(fname, cropdata)
     print('Completed frame {}'.format(i))
-
 
 with open('your_refined.json', 'w') as f:
     json.dump(savedict, f)
-print('Saved NM fits')
-    
-
-firstimagepreds = features
-firstimagepath = 'norm_images/image{}.png'.format(str(numimgs-1).zfill(4))
-firstimg = cv2.imread(firstimagepath)
-fig, ax = plt.subplots()
-ax.imshow(firstimg, cmap='gray')
-for feature in firstimagepreds:
-    x = feature.model.particle.x_p
-    y = feature.model.particle.y_p
-    ax.scatter([x],[y], color='red')
-plt.show()
-
-example = features[0]
-
-print('Example feature')
-print(example.model.particle)
-px = int(np.sqrt(example.data.size))
-pix = (px,px)
-cpix = estimator.pixels
-    
-h = example.model.hologram()
-fig, (ax1, ax2) = plt.subplots(1,2)
-ax1.imshow(np.clip(example.data.reshape(pix)*60, 0, 255), cmap='gray')
-ax2.imshow(np.clip(h.reshape(pix)*60, 0, 255), cmap='gray')
-fig.suptitle('Data, Predicted Hologram')
-plt.show()
+print('Saved fits')
